dafc6e6e36e2fb88d775991bc59976157b33d0d7
Max <max_yarmolenko@hotmail.com>
2018-09-05 20:54:35 +0300

    MyFirstApp

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace MyFirstApp
+﻿using System;
+
+namespace MyFirstApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("my first program");
         }
     }
 }
