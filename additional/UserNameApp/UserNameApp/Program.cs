c7d042bf408fad153a06796f82b0e861981dffa7
Max <max_yarmolenko@hotmail.com>
2018-09-16 19:09:33 +0300

    UserNameApp is done

--- additional/UserNameApp/UserNameApp/Program.cs
+++ additional/UserNameApp/UserNameApp/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace UserNameApp
+﻿using System;
+
+namespace UserNameApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string message = "Enter your name: ";
+            Console.Write(message); 
+
+            string name = Console.ReadLine();
+
+            Console.Write("You entered the name: " + name);
+
+            Console.ReadKey();
         }
     }
 }
